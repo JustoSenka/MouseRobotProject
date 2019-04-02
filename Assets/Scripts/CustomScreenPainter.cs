6a43574669ee55a0836b85ca8d6c4f1a5f6cb0d8
Justas <glodjus@gmail.com>
2019-04-02 02:53:58 +0200

    Project upgraded to latest. Bit more commands, suitable for test runs

--- Assets/Scripts/CustomScreenPainter.cs
+++ Assets/Scripts/CustomScreenPainter.cs
@@ -24,7 +24,7 @@ namespace RobotEditor.Resources.ScriptTemplates
         public void OnPaint(PaintEventArgs e)
         {
             var g = e.Graphics;
-            g.DrawString("Test text", Fonts.Big, Brushes.Red, new PointF(100, 300));
+            g.DrawString("Test lol 2", Fonts.Big, Brushes.Red, new PointF(100, 300));
         }
     }
 }
